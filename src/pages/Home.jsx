664309c922877a6cb31fa20c42a59a1fa780c573
Stephani Engel <stephaniengel18@gmaile.com>
2024-01-02 10:47:18 -0300

    training life cycle, request for api with axios, grid and ui material components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import PokemonCard from "../components";
+import { Container, Grid } from "@mui/material";
+import axios from "axios";
+import { Skeletons } from "../components/Skeletons";
+
+export default function Home() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    let endpoints = [];
+    for (let i = 1; i < 50; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+
+    axios
+      .all(endpoints.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setPokemons(res))
+      .catch((err) => console.log(err));
+  };
+
+  const pokemonFilter = (name) => {
+    let filteredPokemons = [];
+    if (name === "") {
+      getPokemons();
+    }
+    for (let i in pokemons) {
+      if (pokemons[i].data.name.includes(name)) {
+        filteredPokemons.push(pokemons[i]);
+      }
+    }
+    setPokemons(filteredPokemons);
+  };
+
+  return (
+    <div>
+      <Navbar pokemonFilter={pokemonFilter} />
+      <Container maxWidth="false">
+        <Grid container spacing={3}>
+          {pokemons.length === 0 ? (
+            <Skeletons />
+          ) : (
+            pokemons.map((pokemon, key) => (
+              <Grid item xs={12} s={6} md={4} lg={2} key={key}>
+                <PokemonCard
+                  name={pokemon.data.name}
+                  image={pokemon.data.sprites.front_default}
+                  types={pokemon.data.types}
+                />
+              </Grid>
+            ))
+          )}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
